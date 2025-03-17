137e110c92451b4ec926b5d4457c3d1176bc40fe
TLG <tlghealthy@gmail.com>
2025-03-16 19:40:59 -0700

    Update packaging script

--- make_installer.ps1
+++ make_installer.ps1
@@ -1,1 +1,5 @@
-pyinstaller --onefile sfs.py --name StarfieldStorm
+
+$timestamp = Get-Date -Format "yyyyMMdd_HHmmss"
+# pyinstaller --onefile --name "my_script_$timestamp" my_script.py
+
+pyinstaller --add-data "settings.json;." --add-data "assets/*;assets" --onefile sfs.py --name "StarfieldStorm_$timestamp"
